691bca28e62397efdf66807296b40dc3a5bfa192
Pau MB <paumb85@gmail.com>
2024-11-25 18:12:28 +0100

    #6 lint smart contract

--- blockchain/src/user/UserConstant.sol
+++ blockchain/src/user/UserConstant.sol
@@ -4,8 +4,7 @@ pragma solidity 0.8.27;
 contract UserConstant {
     bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
     bytes32 public constant RAW_MINERAL_ROLE = keccak256("RAW_MINERAL_ROLE");
-    bytes32 public constant JEWEL_FACTORY_ROLE =
-        keccak256("JEWEL_FACTORY_ROLE");
+    bytes32 public constant JEWEL_FACTORY_ROLE = keccak256("JEWEL_FACTORY_ROLE");
     bytes32 public constant DISTRIBUTOR_ROLE = keccak256("DISTRIBUTOR_ROLE");
     bytes32 public constant STORE_ROLE = keccak256("STORE_ROLE");
 }
