15bb72799d67a71740efae8b73c8434a598f4937
Johan Sebastian Rodas <sebastian.rbcn@gmail.com>
2024-12-01 10:17:24 +0100

    :sparkles: add JewelFactory__Deploy.s.sol and Distributor__Deploy.s.sol

--- blockchain/script/All_Contracts__Deploy.s.sol
+++ blockchain/script/All_Contracts__Deploy.s.sol
@@ -5,6 +5,7 @@ import {Script} from "forge-std/Script.sol";
 import {UserJewelChain} from "../src/user/UserJewelChain.sol";
 import {RawMineral} from "../src/jewel-chain/RawMineral.sol";
 import {Distributor} from "../src/distributor/Distributor.sol";
+import {JewelFactory} from "../src/jewel-chain/JewelFactory.sol";
 
 contract DeployAllContracts is Script {
     function run() external {
@@ -22,8 +23,14 @@ contract DeployAllContracts is Script {
         Distributor distributor = new Distributor(rawMineralAddress, userJewelChainAddress);
         address distributorAddress = address(distributor);
 
-        // seteamos address distributor a raw_minerl
+        // desplegamos JEWEL_FACTORY
+        JewelFactory jewelFactory = new JewelFactory(
+            userJewelChainAddress, rawMineralAddress, distributorAddress, "https://api.jewelchain.com/metadata/"
+        );
+
+        // Configurar las referencias cruzadas
         rawMineral.setDistributorSC(distributorAddress);
+        distributor.setJewelFactoryAddress(address(jewelFactory));
 
         vm.stopBroadcast();
     }
--- blockchain/script/JewelFactory__Deploy.s.sol
+++ blockchain/script/JewelFactory__Deploy.s.sol
@@ -7,14 +7,14 @@ import {JewelFactory} from "../src/jewel-chain/JewelFactory.sol";
 contract DeployJewelFactory is Script {
     function run() external {
         vm.startBroadcast();
-        
+
         address userJewelChain = 0x31BFc5d6C8FfA2F83e0d7f125C49047de3f76fb6;
         address rawMineral = 0x399D46544Ea7E29A6854a9533d6adccEf8DD5097;
-        address distributor = 0x3DEcAFd0454f55BEfc5574af43dc4860e9ad7eB4;
+        address distributor = 0xea60FB44a2C8b7De3549F786EecFf877A226Ce62;
         string memory baseUri = "https://api.jewelchain.com/metadata/";
-        
+
         new JewelFactory(userJewelChain, rawMineral, distributor, baseUri);
-        
+
         vm.stopBroadcast();
     }
-} 
+}
